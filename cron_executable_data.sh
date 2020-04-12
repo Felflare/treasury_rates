c67e5ca30af313e984d749b1bcdd5a11b8951d99
Ubuntu <ubuntu@ip-172-31-75-21.ec2.internal>
2020-04-12 20:22:55 +0000

    added better cron serup

--- cron_executable_data.sh
+++ cron_executable_data.sh
@@ -2,10 +2,10 @@
 PATH=/home/ubuntu/anaconda3/bin:/home/ubuntu/bin:/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:sbin:/bin
 
 
-
+date
 cd /app/app/data_pull
 python pull_data.py
 python process_csv.py
-sleep 5
-ps -ef
+echo "Stopping old servers"
+killall gunicorn
 echo ""
--- cron_executable_server.sh
+++ cron_executable_server.sh
@@ -2,12 +2,11 @@
 PATH=/home/ubuntu/anaconda3/bin:/home/ubuntu/bin:/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:sbin:/bin
 
 
-echo "Stopping old servers"
-killall gunicorn
-sleep 10
-cd /app
-echo "Starting servers"
-bash start.sh
-sleep 5
-ps -ef
-echo ""
+if ! pgrep gunicorn >/dev/null
+then
+     cd /app
+     echo "Starting servers"
+     bash start.sh
+     ps -ef
+     echo ""
+fi
